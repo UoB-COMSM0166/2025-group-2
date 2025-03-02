481464a71e73c88137db07b88172e9f25a5c4f61
doris <doris@wishingsoft.com>
2025-03-02 00:33:31 +0000

    feat: implement GameUIManager and Shop classes for game UI management

--- docs/shop/index.js
+++ docs/shop/index.js
@@ -1,5 +1,6 @@
 export { BombFruit } from './BombFruit.js';
-export { DoubleScoreTool } from './DoubleScoreTool.js';
 export { DivineShieldTool } from './DivineShieldTool.js';
+export { DoubleScoreTool } from './DoubleScoreTool.js';
 export { RainbowFruit } from './RainbowFruit.js';
+export { Shop } from './Shop.js';
 export { ShuffleTool } from './ShuffleTool.js';
