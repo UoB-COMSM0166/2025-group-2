583090c4988d789e6475a03d8314927093539d36
Hayley13 <hayleytay13@outlook.com>
2025-03-18 22:18:14 +0000

    #172 adding on/off toggle for tutorials
    added on/off toggle in menu page and also button to skip tutorial in game page

--- docs/core/Game.js
+++ docs/core/Game.js
@@ -11,35 +11,69 @@ export class Game {
 		this.scaleVal = scaleVal;
 		this.tutorialManager = null;
 		this.isTutorialMode = false;
+		this.tutorialEnabled = true; // Default: tutorial enabled
+	}
+
+	// Set tutorial preference
+	setTutorialEnabled(enabled) {
+		this.tutorialEnabled = enabled;
 	}
 
 	startGame(mode) {
 		console.log(`Starting game in ${mode} mode!`);
-		this.currentScene = 'tutorial'; // Start with tutorial instead of game
-		this.isTutorialMode = true;
-		this.menuPage.hideButtons();
 
-		// Initialize GameManager but don't start the game yet
-		this.gameManager = new GameManager(this, mode, this.scaleVal);
-
-		// Make sure the counter is stopped during tutorial
-		if (this.gameManager && this.gameManager.counter) {
-			this.gameManager.counter.stop();
-		}
-
-		// Also stop the uiManager counter
-		if (this.gameManager && this.gameManager.uiManager && this.gameManager.uiManager.counter) {
-			this.gameManager.uiManager.counter.stop();
-		}
-
-		// Create tutorial manager with the game manager instance
-		try {
-			this.tutorialManager = new TutorialManager(this, this.gameManager);
-		} catch (error) {
-			console.error('Error creating tutorial manager:', error);
-			// Fall back to starting the game directly if tutorial fails
+		// Check if tutorial should be shown
+		if (this.tutorialEnabled) {
+			this.currentScene = 'tutorial';
+			this.isTutorialMode = true;
+		} else {
+			// Skip tutorial and go straight to the game
 			this.currentScene = 'game';
 			this.isTutorialMode = false;
+		}
+
+		this.menuPage.hideButtons();
+
+		// Initialize GameManager
+		this.gameManager = new GameManager(this, mode, this.scaleVal);
+
+		if (this.isTutorialMode) {
+			// Make sure the counter is stopped during tutorial
+			if (this.gameManager && this.gameManager.counter) {
+				this.gameManager.counter.stop();
+			}
+
+			// Also stop the uiManager counter
+			if (this.gameManager && this.gameManager.uiManager && this.gameManager.uiManager.counter) {
+				this.gameManager.uiManager.counter.stop();
+			}
+
+			// Create tutorial manager with the game manager instance
+			try {
+				this.tutorialManager = new TutorialManager(this, this.gameManager);
+			} catch (error) {
+				console.error('Error creating tutorial manager:', error);
+				// Fall back to starting the game directly if tutorial fails
+				this.currentScene = 'game';
+				this.isTutorialMode = false;
+			}
+		} else {
+			// Start the counter immediately if tutorial is skipped
+			if (this.gameManager.counter) {
+				this.gameManager.counter.start();
+			}
+			if (this.gameManager.uiManager && this.gameManager.uiManager.counter) {
+				this.gameManager.uiManager.counter.start();
+			}
+
+			// Start incident manager right away
+			if (this.gameManager && this.gameManager.player && this.gameManager.player.length > 0) {
+				this.gameManager.player.forEach(player => {
+					if (player.boards && player.boards.incidentManager) {
+						player.boards.incidentBegin();
+					}
+				});
+			}
 		}
 
 		// Call the mode change callback to notify app.js that the game mode has changed
--- docs/pages/MenuPage.js
+++ docs/pages/MenuPage.js
@@ -7,6 +7,11 @@ export class MenuPage {
 
 		this.doublePlayerButton = createButton('Double Player');
 		this.doublePlayerButton.mousePressed(() => this.game.startGame('double'));
+
+		// Create tutorial toggle
+		this.tutorialEnabled = true; // Default: tutorial enabled
+		this.tutorialToggle = createButton('Tutorial: ON');
+		this.tutorialToggle.mousePressed(() => this.toggleTutorial());
 	}
 
 	display() {
@@ -18,18 +23,31 @@ export class MenuPage {
 
 		this.singlePlayerButton.position(width / 2 - 50, height / 2);
 		this.doublePlayerButton.position(width / 2 - 50, height / 2 + 50);
+		this.tutorialToggle.position(width / 2 - 50, height / 2 + 100);
 
 		this.singlePlayerButton.show();
 		this.doublePlayerButton.show();
+		this.tutorialToggle.show();
 	}
 
 	hideButtons() {
 		this.singlePlayerButton.hide();
 		this.doublePlayerButton.hide();
+		this.tutorialToggle.hide();
 	}
 
 	showButtons() {
 		this.singlePlayerButton.show();
 		this.doublePlayerButton.show();
+		this.tutorialToggle.show();
+	}
+
+	// Toggle tutorial state and update button text
+	toggleTutorial() {
+		this.tutorialEnabled = !this.tutorialEnabled;
+		this.tutorialToggle.html(this.tutorialEnabled ? 'Tutorial: ON' : 'Tutorial: OFF');
+
+		// Update the game's tutorial preference
+		this.game.setTutorialEnabled(this.tutorialEnabled);
 	}
 }
