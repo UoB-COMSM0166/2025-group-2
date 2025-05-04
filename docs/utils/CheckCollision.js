41021411ae289923c5f91a5eebc2e2d572987190
shiyufan0820 <shiyufan0820@gmail.com>
2025-05-04 10:30:47 +0100

    Fixed rainbow fruit and bomb fruit, now they are more accurate

--- docs/utils/CheckCollision.js
+++ docs/utils/CheckCollision.js
@@ -1,3 +1,4 @@
 export function checkCollision(a, b) {
-	return dist(a.x, a.y, b.x, b.y) < 2 + a.d;
+	// return dist(a.x, a.y, b.x, b.y) < 2 + a.d;
+	return dist(a.x, a.y, b.x, b.y) <= a.d / 2 + b.d / 2;
 }
